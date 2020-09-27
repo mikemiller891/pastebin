e802ab9884f0957e788d3ce7345a20597753a83c
Mike Miller <mike@reslar.com>
2020-09-27 01:23:13 -0500

    Refactored the buttons into components.

---
+++ app/View/Components/Button.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+    public $color;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($color)
+    {
+        $this->color = $color;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return <<<'blade'
+        <button class="uppercase text-l bg-{{ $color }}-500
+          hover:bg-{{ $color }}-700 dark:hover:bg-{{ $color }}-300 text-black
+          dark:text-white hover:text-white dark:hover:text-black font-bold
+          py-1 px-4 rounded-full opacity-50 hover:opacity-100" type="submit" name="cmd" {{ $attributes }}>{{ $slot }}</button>
+blade;
+    }
+}
--- resources/views/paste/show.blade.php
+++ resources/views/paste/show.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
 
-    <form class="h-full w-full overflow-hidden" action="/{{ $key }}" method="post">@csrf
-        <div class="h-full w-full bg-white dark:bg-black text-black dark:text-white text-xl font-mono resize-none p-4 whitespace-pre-wrap overflow-y-scroll">{{ $content }}</div>
+    <form class="h-full w-full overflow-hidden" action="/{{ $key }}"
+      method="post">@csrf
+        <div class="h-full w-full bg-white dark:bg-black text-black
+          dark:text-white text-l font-mono resize-none p-4 whitespace-pre-wrap
+          overflow-y-scroll">{{ $content }}</div>
 
         <div class="absolute bottom-0 right-0 pr-8 pb-4 bg-transparent">
-            <button class="text-2xl bg-blue-500 hover:bg-blue-700 dark:hover:bg-blue-300 text-black dark:text-white hover:text-white dark:hover:text-black font-bold py-2 px-4 rounded-full opacity-50 hover:opacity-100" type="submit" name="cmd" value="fork">Fork</button>
-            <button class="text-2xl bg-orange-500 hover:bg-orange-700 dark:hover:bg-orange-300 text-black dark:text-white hover:text-white dark:hover:text-black font-bold py-2 px-4 rounded-full opacity-50 hover:opacity-100" type="submit" name="cmd" value="new">New</button>
+            <x-button color="blue" value="fork">Fork</x-button>
+            <x-button color="orange" value="new">New</x-button>
         </div>
     </form>
 
