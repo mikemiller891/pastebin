daff2ac59dcee7ee79f653c7fa73571c136bb92a
Mike Miller <mike@reslar.com>
2020-10-14 13:10:30 -0500

    Added migration file to create the pastes table in the database.

---
+++ database/migrations/2020_10_14_022717_create_pastes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePastesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pastes', function (Blueprint $table) {
+            $table->char('key', env('PASTE_KEY_LENGTH')); // Randomly generated alpha string
+            $table->text('content');
+
+            $table->id(); // Not used. The field 'key' is used to identify a record.
+            $table->timestamps(); // Also not currently used.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pastes');
+    }
+}
