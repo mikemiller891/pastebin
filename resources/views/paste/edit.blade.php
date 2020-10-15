682912f91ec4ac935865373a4cc77bfc81f23c3b
Mike Miller <mike@reslar.com>
2020-10-15 09:13:09 -0500

    Fixed appearance of buttons when focused.

--- resources/views/paste/edit.blade.php
+++ resources/views/paste/edit.blade.php
@@ -6,8 +6,8 @@
             <textarea name="content" autofocus class="h-full w-full font-mono resize-none p-4 whitespace-pre-wrap overflow-y-scroll bg-black text-white">{{ $content ?? '' }}</textarea>
         </div>
         <div class="absolute bottom-0 right-0 pb-4 pr-8">
-            <button name="action" value="save" class="bg-green-500 text-black hover:text-white py-0 px-4 rounded-full opacity-50 hover:opacity-100 font-black">Save</button>
-            <button name="action" value="cancel" class="bg-red-500 text-black hover:text-white py-0 px-4 rounded-full opacity-50 hover:opacity-100 font-black">Cancel</button>
+            <button name="action" value="save" class="bg-green-500 text-black hover:text-white focus:text-white py-0 px-4 rounded-full opacity-50 hover:opacity-100 focus:opacity-100 font-black focus:outline-none">Save</button>
+            <button name="action" value="cancel" class="bg-red-500 text-black hover:text-white focus:text-white py-0 px-4 rounded-full opacity-50 hover:opacity-100 focus:opacity-100 font-black focus:outline-none">Cancel</button>
         </div>
     </form>
 @endsection
